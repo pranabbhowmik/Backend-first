0e6cb79e4b91eb81973688352c5b4e88936601b8
Pranab Bhowmik <109957935+Pranab09122002@users.noreply.github.com>
2024-10-27 21:46:53 +0530

    Add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const subsciptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Subsciption = mongoose.model("Subsciption", subsciptionSchema);
